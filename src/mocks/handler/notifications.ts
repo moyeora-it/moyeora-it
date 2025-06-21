1ccc5a783b857e970f3d2c3f1dc14b712e708b18
Lee Yuna <leeyuna7689@gmail.com>
2025-06-22 01:16:05 +0900

    fix: Notification 타입의 createdDate의 타입을 Date로 돌려놓음
    Notification 타입의 createdDate의 타입을 Date로 돌려놓고 formatRelativeTime에 문자화한 값을 넣고 호출

--- src/mocks/handler/notifications.ts
+++ src/mocks/handler/notifications.ts
@@ -1,7 +1,7 @@
-import { eNotification } from '@/types/enums';
-import { http, HttpResponse } from 'msw';
-import { InfiniteResponse } from '@/types/response';
 import { Notification } from '@/types';
+import { eNotification } from '@/types/enums';
+import { InfiniteResponse } from '@/types/response';
+import { http, HttpResponse } from 'msw';
 
 export const NOTIFICATIONS = [
   {
@@ -22,33 +22,39 @@ export const NOTIFICATIONS = [
 ];
 
 export const notificationsHandlers = [
-  http.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/notifications`, ({ request }) => {
-    const url = new URL(request.url);
-    const searchParams = new URLSearchParams(url.search);
-    const isRead = searchParams.get('isRead');
+  http.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/notifications`,
+    ({ request }) => {
+      const url = new URL(request.url);
+      const searchParams = new URLSearchParams(url.search);
+      const isRead = searchParams.get('isRead');
 
-    const filteredNotifications = NOTIFICATIONS.filter((notification) => {
-      return isRead === 'false' ? !notification.isRead : true;
-    }).map(notification => ({
-      ...notification,
-      createdAt: new Date(notification.createdAt),
-      url: notification.url ?? null
-    }));
+      const filteredNotifications = NOTIFICATIONS.filter((notification) => {
+        return isRead === 'false' ? !notification.isRead : true;
+      }).map((notification) => ({
+        ...notification,
+        createdAt: new Date(notification.createdAt),
+        url: notification.url ?? null,
+      }));
 
-    return HttpResponse.json<InfiniteResponse<Notification>>({
-      status: {
-        code: 200,
-        message: 'success',
-        success: true
-      },
-      data: filteredNotifications,
-      hasNext: false,
-      cursor: 0,
-    });
-  }),
-  http.get(`${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/notifications/unread-count`, () => {
-    return HttpResponse.json({
-      unreadCount: 1
-    });
-  }),
+      return HttpResponse.json<InfiniteResponse<Notification>>({
+        status: {
+          code: 200,
+          message: 'success',
+          success: true,
+        },
+        data: filteredNotifications,
+        hasNext: false,
+        cursor: 0,
+      });
+    },
+  ),
+  http.get(
+    `${process.env.NEXT_PUBLIC_API_BASE_URL}/v1/notifications/unread-count`,
+    () => {
+      return HttpResponse.json({
+        unreadCount: 1,
+      });
+    },
+  ),
 ];
