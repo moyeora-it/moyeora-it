d32058fc402059cbf07c62c7496141e1a090b110
Kim Kangmin <rlarkdals1202@gmail.com>
2025-06-23 10:21:48 +0900

    fix: 불필요한 import문 제거

--- src/features/user/components/withdraw-dialog.tsx
+++ src/features/user/components/withdraw-dialog.tsx
@@ -3,7 +3,6 @@
 import { useState } from 'react';
 import { useRouter } from 'next/navigation';
 import { toast } from 'sonner';
-import { Button } from '@/components/ui/button';
 import {
   AlertDialog,
   AlertDialogTrigger,
